b8e0214d52f74b8c304fc3a437555834daf6c00a
dafine lira <dafinelirar@gmail.com>
2024-07-27 18:19:33 -0300

    criando parametro usando query params

--- src/server.js
+++ src/server.js
@@ -6,7 +6,14 @@ app.get("/message/:id/:user", (request, response) => {
     const { id, user} = request.params
 
     response.send(`Id da mensagem: ${id},
-        Para o usuário: ${user}`)
+        Para o usuário: ${user}`);
+});
+
+app.get("/users", (request, response) => {
+    const {page, limit} = request.query
+
+    response.send (`Página: ${page}. Mostrar: ${limit}`)
+
 })
 
 const PORT = 3333;
