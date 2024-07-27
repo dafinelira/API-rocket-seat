dd4627ed64658317656ad87babdc1b3377db386d
dafine lira <dafinelirar@gmail.com>
2024-07-27 18:07:54 -0300

    desestruturando request.params

--- src/server.js
+++ src/server.js
@@ -3,8 +3,10 @@ const express = require ('express');
 const app = express();
 
 app.get("/message/:id/:user", (request, response) => {
-    response.send(`Id da mensagem: ${request.params.id},
-        Para o usuário: ${request.params.user}`)
+    const { id, user} = request.params
+
+    response.send(`Id da mensagem: ${id},
+        Para o usuário: ${user}`)
 })
 
 const PORT = 3333;
