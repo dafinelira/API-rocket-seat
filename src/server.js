dabbd58bd4ef821abb2e0193d10976da87c46bf5
dafine lira <dafinelirar@gmail.com>
2024-07-27 17:33:08 -0300

    adicionando o método get

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const express = require ('express');
 
 const app = express();
 
+app.get("/", (request, response) => {
+    response.send("Hello Word")
+})
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`server is runing on port ${PORT}`));
 
