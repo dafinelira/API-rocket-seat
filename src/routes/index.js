87bd6b79b2904ccd708499def4ab37f750e23487
dafine lira <dafinelirar@gmail.com>
2024-07-28 10:21:32 -0300

    dividindo o código em arquivos para melhor organização

---
+++ src/routes/index.js
@@ -0,0 +1,6 @@
+const { Router } = require("express");
+
+const usersRoutes = require("./users.routes")
+const routes = Router();
+
+routes.use("/users", usersRoutes)
--- src/server.js
+++ src/server.js
@@ -1,14 +1,9 @@
 const express = require ('express');
 
+
 const app = express();
 
 app.use(express.json());
-
-app.post("/users", (request, response) => {
-    const{name, email, password} = request.body;
-    
-    response.json ({name, email, password})
-})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`server is runing on port ${PORT}`));
