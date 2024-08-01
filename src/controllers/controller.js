79a4b8a0ca5bfa1649e23936472a78bf316c332b
dafine lira <dafinelirar@gmail.com>
2024-08-01 20:52:07 -0300

    adicionando controllers

---
+++ src/controllers/controller.js
@@ -0,0 +1,14 @@
+class UserController {
+/**
+ * como boa prática: ATÉ 5 funções
+ * 1- index - GET para listar vários registros
+ * 2- show - GET para exibir registro especifico
+ * 3 - create - POST para criar um registro
+ * 4- update - PUT para atualizar um registo.
+ * 5- delete - DELETE para remover um registro. 
+ */
+
+
+}
+
+module.exports = UsersController;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require ('express');
 
+const routes = require("./routes")
 
 const app = express();
 
 app.use(express.json());
+
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`server is runing on port ${PORT}`));
