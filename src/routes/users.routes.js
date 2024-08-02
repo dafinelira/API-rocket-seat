53b9da943724706a492331585a714bd80037e295
dafine lira <dafinelirar@gmail.com>
2024-08-01 21:18:11 -0300

    criando a function myMiddleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,8 +4,18 @@ const UsersController = require("../controllers/controller")
 
 const usersRoutes = Router();
 
+function myMiddleware (request, response, next){
+    console.log("você passou pele middleware");
+    
+    next();
+}
+
+
+
+
+
 const usersController = new UsersController();
 
-usersRoutes.post("/", usersController.create);
+usersRoutes.post("/", myMiddleware,usersController.create);
 
 module.exports = usersRoutes;
