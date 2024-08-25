05acd85ddeb3dab323e9fd3b5cdbc7d293b2903c
dafine lira <dafinelirar@gmail.com>
2024-08-25 19:06:31 -0300

    migrations para link e tag

--- src/database/knex/migrations/20240821004130_createTags.js
+++ src/database/knex/migrations/20240821004130_createTags.js
@@ -4,9 +4,9 @@ exports.up = knex => knex.schema.createTable("tags", table => {
 
     table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");
     table.integer("user_id").references("id").inTable("users");
-        
+          
 });
 
 
-exports.down = knex => knex.schema.createTable("tags");
+exports.down = knex => knex.schema.dropTable("tags");
 
